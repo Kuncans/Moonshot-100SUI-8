f34c0ef685d3f46409ec99f4d7a584d087e07512
Kuncans <akafauna@gmail.com>
2022-03-13 23:21:24 +0000

    Initial Commit

---
+++ Moonshot-100SUI-8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot-100SUI-8
+//
+//  Created by Duncan Kent on 13/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moonshot-100SUI-8/Moonshot_100SUI_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_100SUI_8App.swift
+//  Moonshot-100SUI-8
+//
+//  Created by Duncan Kent on 13/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_100SUI_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
