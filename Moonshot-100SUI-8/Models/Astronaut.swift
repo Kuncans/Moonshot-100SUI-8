578a19389043485371a752e1c96309afbd966489
Kuncans <akafauna@gmail.com>
2022-03-14 00:16:55 +0000

    Added assets and models

---
+++ Moonshot-100SUI-8/Models/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot-100SUI-8
+//
+//  Created by Duncan Kent on 13/03/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot-100SUI-8/Models/Mission.swift
@@ -0,0 +1,29 @@
+//
+//  Mission.swift
+//  Moonshot-100SUI-8
+//
+//  Created by Duncan Kent on 13/03/2022.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+}
